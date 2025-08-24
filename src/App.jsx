f1ba9c16336e1889690a62127d8e4c6ebc12aa26
anshikayadav11 <anshikayadav2917@gmail.com>
2025-08-24 21:40:31 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Login from'./UserComponent'
+
+function App(){
+   return(
+      <div>
+         <h1>Importing and Exporting Component</h1>
+         <Login />
+      </div>
+   )
+}
+
+export default App;
