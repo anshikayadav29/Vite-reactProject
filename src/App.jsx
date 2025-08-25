cac065a8e4001baea02547cc4ddbcf69e514562b
anshikayadav11 <anshikayadav2917@gmail.com>
2025-08-26 04:18:06 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,47 @@
-import Login from'./UserComponent'
+/*import Login,{Profile,Setting,Userkey} from'./UserComponent'
 
 function App(){
    return(
       <div>
          <h1>Importing and Exporting Component</h1>
          <Login />
+         <Profile />
+         <Setting />
+         <h1>{Userkey}</h1>
       </div>
    )
+}
+
+export default App;*/
+
+/*import ToDo from "./ToDo"
+function App(){
+  return(
+    <div>
+
+    <h1>JSX Exercise</h1>
+    <ToDo />
+    </div>
+  )
+}
+export default App*/
+
+
+
+
+
+function App() {
+  const name = "Anshika yadav"; 
+  let x=20;
+  let y=40;
+  return (
+    <div>
+      <h1>JSX with Curly Braces</h1>
+      <h1>{name}</h1> 
+      <h1>{x+y}</h1> 
+
+    </div>
+  );
 }
 
 export default App;
