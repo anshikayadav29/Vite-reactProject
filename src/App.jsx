3f63f1d9f6b35e81f431c9077bd7eaff0dd2e967
anshikayadav11 <anshikayadav2917@gmail.com>
2025-09-03 01:15:19 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -232,7 +232,7 @@ function App() {
 export default App*/
 //count
 
-import { useState } from "react"
+/*import { useState } from "react"
 function App(){
   const [count,setCount]=useState(1);
   return(
@@ -241,6 +241,25 @@ function App(){
     <button onClick={()=>setCount(count+1)} >Counter</button>
     {
       count==0?<h1>Condition 1</h1>:null
+    }
+    </div>
+  )
+}
+export default App*/
+import { useState } from "react"
+function App(){
+  const [count,setCount]=useState(3);
+  return(
+    <div>
+    <h1>{count}</h1>
+    <button onClick={()=>setCount(count+1)} >Counter</button>
+    {
+      count==0?<h1>Condition 0</h1>
+      :count==1?<h1>Condition 1</h1>
+      :count==2?<h1>Condition 2</h1>
+      :count==3?<h1>Condition 3</h1>
+      :count==4?<h1>Condition 4</h1>
+      :null  
     }
     </div>
   )
