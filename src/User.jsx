cc3db0e1f9ee782bf530e2d71a065efc07cf4c67
anshikayadav11 <anshikayadav2917@gmail.com>
2025-09-06 02:47:38 +0530

    commit

--- src/User.jsx
+++ src/User.jsx
@@ -4,7 +4,7 @@
 
 export default User;*/
 
-function User({name,age,email}) {
+/*function User({name,age,email}) {
   return(
     <div>
      <h2>Name:{name}</h2>
@@ -15,5 +15,18 @@ function User({name,age,email}) {
   )  
 }
 
+export default User;*/
+function User({ user }) {
+  return (
+    <div>
+      <hr />
+      <h2>Name: {user.name}</h2>
+      <h2>Age: {user.age}</h2>
+      <h2>Email: {user.email}</h2>
+    </div>
+  )
+}
+
 export default User;
+
 
