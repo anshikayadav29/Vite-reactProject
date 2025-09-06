3c3501445bd649828b0a4d5e432558f941c48728
anshikayadav11 <anshikayadav2917@gmail.com>
2025-09-06 23:53:06 +0530

    commit

---
+++ src/Wrapper.jsx
@@ -0,0 +1,19 @@
+/*function Wrapper() {
+  return (
+    <div style={{ color: "green", border: "5px solid green", width: "200px" }}>
+      <h1>Hello Everyone</h1>
+    </div>
+  )
+}
+
+export default Wrapper;*/
+function Wrapper({ color = "green", children }) {
+  return (
+    <div style={{ color: color, border: `5px solid ${color}`, width: "250px", margin: "10px", padding: "10px" }}>
+      {children}
+    </div>
+  )
+}
+
+export default Wrapper
+
