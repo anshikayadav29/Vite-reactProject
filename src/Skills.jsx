407239c4f01b861819f516ee6da8a1c0f5ad599e
anshikayadav11 <anshikayadav2917@gmail.com>
2025-09-09 00:49:51 +0530

    Commit

---
+++ src/Skills.jsx
@@ -0,0 +1,63 @@
+/*function Skills() {
+  return (
+    <div>
+      <h3>Select Your Skills</h3>
+      <input type="checkbox" id="php" />
+      <label htmlFor="php">PHP</label>
+      <br />
+      <br />
+      <input type="checkbox" id="php" />
+      <label htmlFor="php">PHP</label>
+      <br />
+      <br />
+
+      <input type="checkbox" id="php" />
+      <label htmlFor="php">PHP</label>
+      <br />
+      <br />
+
+      <input type="checkbox" id="php" />
+      <label htmlFor="php">PHP</label>
+      <br />
+      <br />
+
+      <input type="checkbox" id="php" />
+      <label htmlFor="php">PHP</label>
+      
+    </div>
+  );
+}
+
+export default Skills;*/
+
+function Skills() {
+  return (
+    <div>
+      <h3>Select Your Skills</h3>
+      <input type="checkbox" id="php" value="php" />
+      <label htmlFor="php">PHP</label>
+      <br />
+      <br />
+      <input type="checkbox" id="php" />
+      <label htmlFor="js">JS</label>
+      <br />
+      <br />
+
+      <input type="checkbox" id="php" />
+      <label htmlFor="node">Node</label>
+      <br />
+      <br />
+
+      <input type="checkbox" id="node" value="node" />
+      <label htmlFor="node">NODE</label>
+      <br />
+      <br />
+
+      <input type="checkbox" id="java" value="java" />
+      <label htmlFor="java">JAVA</label>
+    </div>
+  );
+}
+
+export default Skills;
+
