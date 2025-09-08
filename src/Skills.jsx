15e5a8833db438c4bd02dca203128d42aa4b757d
anshikayadav11 <anshikayadav2917@gmail.com>
2025-09-09 00:57:26 +0530

    commit

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -30,7 +30,7 @@
 
 export default Skills;*/
 
-function Skills() {
+/*function Skills() {
   return (
     <div>
       <h3>Select Your Skills</h3>
@@ -59,5 +59,41 @@ function Skills() {
   );
 }
 
+export default Skills;*/
+//SOME CHANGE
+
+import { useState } from "react";
+function Skills() {
+  const [skills,setSkills]=useState(["java","php"])
+  return (
+    <div>
+      <h3>Select Your Skills</h3>
+      <input type="checkbox" id="php" value="php" />
+      <label htmlFor="php">PHP</label>
+      <br />
+      <br />
+      <input type="checkbox" id="php" />
+      <label htmlFor="js">JS</label>
+      <br />
+      <br />
+
+      <input type="checkbox" id="php" />
+      <label htmlFor="node">Node</label>
+      <br />
+      <br />
+
+      <input type="checkbox" id="node" value="node" />
+      <label htmlFor="node">NODE</label>
+      <br />
+      <br />
+
+      <input type="checkbox" id="java" value="java" />
+      <label htmlFor="java">JAVA</label>
+      <h1>{skills.toString()}</h1>
+    </div>
+  );
+}
+
 export default Skills;
+
 
