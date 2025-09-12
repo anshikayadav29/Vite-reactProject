82d838bee2d6e885b6da46391ceca7e892cf17cd
anshikayadav11 <anshikayadav2917@gmail.com>
2025-09-13 04:26:23 +0530

    commit

--- src/College.jsx
+++ src/College.jsx
@@ -1,9 +1,30 @@
-function College() {
+/*function College() {
   return (
     <div>
       <h1>College</h1>
     </div>
   )
+}
+
+export default College;
+*/
+
+import React from "react";
+
+function College({ name, city, website, students }) {
+  return (
+    <div style={{ background: "#ddd", padding: "20px", margin: "20px 0" }}>
+      <h2>Name: {name}</h2>
+      <p><b>City:</b> {city}</p>
+      <p><b>Website:</b> {website}</p>
+      <p><b>Students</b></p>
+      <ul>
+        {students.map((stu, index) => (
+          <li key={index}>Name : {stu}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default College;
